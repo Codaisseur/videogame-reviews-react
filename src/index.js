3ea9cec09a18d66e46c509e1b754402fc72c5f79
Wouter de Vos <wouter@codaisseur.com>
2016-04-28 15:28:01 +0200

    Basic routes with a PageNotFound component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router';
 import App from './App';
+import PageNotFound from './PageNotFound';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <Route path="*" component={PageNotFound}/>
+    </Route>
+  </Router>
+), document.getElementById('root'));
